65d8bd639136be16e38c79613d75ca47d3d45c3c
Deepank Sharma <gaurdeepank0@gmail.com>
2025-07-20 01:02:47 +0530

    Import bugs removed

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { useState} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import { useSearchParams } from 'react-router-dom';
-import { addToPastes, updateToPastes } from '../redux/pasteSlice';
+import { addToPastes, updateToPastes } from '../Redux/pasteSlice';
 
 const HomePage = () => {
 
